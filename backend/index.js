3a412211131e1ae7351121a19ef38394a75404f3
Arti Barsagade <artibarsagade2000@gmail.com>
2026-05-16 21:50:05 +0530

    fix: added render URL to CORS and root route

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,8 @@ const PORT = process.env.PORT || 5000
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin:['http://localhost:5173', 'http://localhost:5174'],
+  origin:['http://localhost:5173', 'http://localhost:5174',   "https://ai-website-yddy.onrender.com"],
+
   credentials: true,
 }))
 app.use('/api', authRouter)
